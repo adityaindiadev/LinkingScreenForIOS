9d2330204be4edf6e71d3ae591f3ed3bb5979cfb
“adityaindiadev” <aditya.india.dev@gmail.com>
2023-08-24 18:58:11 +0530

    first commit

--- ios/LinkingScreenForIOS/AppDelegate.mm
+++ ios/LinkingScreenForIOS/AppDelegate.mm
@@ -2,6 +2,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <UIKit/UIKit.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
